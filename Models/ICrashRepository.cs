467d0c48224b7bfe7382d3fd94cb6f848eee5524
Stephen Williams <williste@byu.edu>
2022-04-04 11:08:47 -0600

    added models and controllers

---
+++ Models/ICrashRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEX_II.Models
+{
+    public interface ICrashRepository
+    {
+        IQueryable<Crash> Crashes { get; }
+        public void SaveCrash(Crash c);
+        public void CreateCrash(Crash c);
+        public void DeleteCrash(Crash c);
+    }
+}
