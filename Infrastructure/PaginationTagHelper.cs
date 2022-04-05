754e40b838259f8438f9abaf580bd2c8b7d2ced7
Stephen Williams <williste@byu.edu>
2022-04-04 20:08:37 -0600

    fixed pagination

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -33,11 +33,44 @@ namespace INTEX_II.Infrastructure
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            int start;
+            int last;
+            if (PageLinks.CurrentPage <= 2)
+            {
+                start = 1;
+                last = 5;
+            }
+            else if (PageLinks.CurrentPage >= PageLinks.TotalPages - 1)
+            {
+                start = PageLinks.TotalPages - 4;
+                last = PageLinks.TotalPages;
+            }
+            else
+            {
+                start = PageLinks.CurrentPage - 2;
+                last = PageLinks.CurrentPage + 2;
+            }
+
             IUrlHelper uh = uhf.GetUrlHelper(vc);
 
             TagBuilder final = new TagBuilder("div");
 
-            for (int i = 1; i <= PageLinks.TotalPages; i++)
+            TagBuilder firstTb = new TagBuilder("a");
+
+            firstTb.Attributes["href"] = uh.Action(PageAction, new { pageNum = 1 });
+
+            if (PageClassesEnabled)
+            {
+                firstTb.AddCssClass(PageClass);
+                firstTb.AddCssClass(1 == PageLinks.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+            }
+
+            firstTb.InnerHtml.Append("First");
+
+            final.InnerHtml.AppendHtml(firstTb);
+
+            for (int i = start; i <= last; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
 
@@ -54,6 +87,21 @@ namespace INTEX_II.Infrastructure
 
                 final.InnerHtml.AppendHtml(tb);
             }
+
+            TagBuilder lastTb = new TagBuilder("a");
+
+            lastTb.Attributes["href"] = uh.Action(PageAction, new { pageNum = PageLinks.TotalPages });
+
+            if (PageClassesEnabled)
+            {
+                lastTb.AddCssClass(PageClass);
+                lastTb.AddCssClass(PageLinks.TotalPages == PageLinks.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+            }
+
+            lastTb.InnerHtml.Append("Last");
+
+            final.InnerHtml.AppendHtml(lastTb);
 
             output.Content.AppendHtml(final.InnerHtml);
         }
