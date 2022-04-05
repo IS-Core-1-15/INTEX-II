70d2287d5d767546391cbc43c5e1236648c9705f
Stephen Williams <williste@byu.edu>
2022-04-05 10:02:28 -0600

    fixed edit and delete posts

--- Models/EFCrashRepository.cs
+++ Models/EFCrashRepository.cs
@@ -25,10 +25,12 @@ namespace INTEX_II.Models
         public void DeleteCrash(Crash c)
         {
             _context.Remove(c);
+            _context.SaveChanges();
         }
 
         public void SaveCrash(Crash c)
         {
+            _context.Update(c);
             _context.SaveChanges();
         }
     }
