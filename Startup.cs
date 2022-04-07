ec0511329ac392b0da1cb464b97a0f7427310273
Stephen Williams <williste@byu.edu>
2022-04-07 14:28:45 -0600

    fixed spelling...

--- Startup.cs
+++ Startup.cs
@@ -78,7 +78,7 @@ namespace INTEX_II
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddSingleton(
-                new InferenceSession("modl/model.onnx")
+                new InferenceSession("model/model.onnx")
             );
 
             services.AddServerSideBlazor();
